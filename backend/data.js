18aebcea9776a117bbbcc6824a5e50321c89fe15
Mamadou Sall <salassmmd@gmail.com>
2021-02-28 14:39:36 +0000

    "installing express and some change package.js "

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+const data = {
+
+    products: [
+        {
+            _id: '1',
+            name: 'Salass',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 1200,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Hight quality products',
+        },
+        {
+            _id: '2',
+            name: 'Alassan',
+            category: 'Schirts',
+            image: '/images/p2.jpg',
+            price: 1200,countInStock: 20,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 30,
+            description: 'Hight quality products',
+        },
+        {
+            _id: '3',
+            name: 'Salass',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 120,countInStock: 0,
+            brand: 'Lewis',
+            rating: 3.5,
+            numReviews: 20,
+            description: 'Hight quality products',
+        },
+        {
+            _id: '4',
+            name: 'Oumar',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 120,countInStock: 12,
+            brand: 'Lewis',
+            rating: 2,
+            numReviews: 10,
+            description: 'Hight quality products',
+        },
+        {
+            _id: '5',
+            name: 'Fama',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 120,countInStock: 2,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Hight quality products',
+        },
+    ],
+
+
+};
+export default data;
